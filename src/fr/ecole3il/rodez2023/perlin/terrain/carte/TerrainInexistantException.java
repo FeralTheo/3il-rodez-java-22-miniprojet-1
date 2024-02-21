1701c6eca8141de5397631d98b7790590e7055c7
Feral Theo <feralt@3il.fr>
2024-02-21 22:04:24 +0100

    Ajout d'une exception "TerrainInexistantException

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/carte/TerrainInexistantException.java
@@ -0,0 +1,8 @@
+package fr.ecole3il.rodez2023.perlin.terrain.carte;
+
+public class TerrainInexistantException extends Exception {
+
+    public TerrainInexistantException(String message) {
+        super(message);
+    }
+}
